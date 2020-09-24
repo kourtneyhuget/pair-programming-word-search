8f2c7580c9e10312899ab9104c0cd1bf4d5e00a7
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-09-24 01:46:53 +0000

    pair programming with Graeme and Tyler

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (word.length === 0) {
+        return false;
     }
-}
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        } else {
+            var newArr = [];
+            for (let i = 0; i < letters[0].length; i++) {
+                newArr.push([]);
+            }
+            for (let l = 0; l < letters.length; l++) {
+                for (let v = 0; v < letters[l].length; v++) {
+                    newArr[v].push(letters[l][v]);
+                }
+            }
+        };
+    };
+    let wordsArray = [];
+    for (array in newArr) {
+        wordsArray.push(newArr[array].join(""));
+        let lastIndex = wordsArray.length - 1;
+        if (wordsArray[lastIndex].includes(word)) {
+            return true;
+        }
+    } return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
